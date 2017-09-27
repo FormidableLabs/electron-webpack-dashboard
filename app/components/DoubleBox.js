584bc164ca1c4e9ae6b0043fb3473966bb8832be
Tegan Churchill <churchill.tegan@gmail.com>
2017-09-27 11:17:37 -0700

    Remove nested ternary

--- app/components/DoubleBox.js
+++ app/components/DoubleBox.js
@@ -6,10 +6,13 @@ const zoomInAnimation = keyframes`${zoomIn}`;
 export default styled.div`
   background: #2a2e3b;
   display: flex;
-  flex: ${({ size }) =>
-    size === 'large'
-      ? '2 1 132px'
-      : size === 'small' ? '1 1 100%' : '1 1 60px'};
+  flex: ${({ size }) => {
+    const style = {
+      large: '2 1 132px',
+      small: '1 1 100%'
+    };
+    return style[size] || '1 1 60px';
+  }},
   height: ${({ size }) => (size === 'small' ? '350px' : '')};
   margin: 6px;
   word-wrap: break-word;
