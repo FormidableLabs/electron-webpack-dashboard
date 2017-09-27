df7e25dde0d2ec043bfd51f17ace0d10f920525a
Tegan Churchill <rawrmonstar@users.noreply.github.com>
2017-09-27 13:16:06 -0700

    Fix CI (#37)
    * Remove nested ternary
    * Fix expected window title
    * Fix font curruption by git
    * Update woff2 mimetype
    * Remove unused e2e tests

--- webpack.config.renderer.dev.dll.js
+++ webpack.config.renderer.dev.dll.js
@@ -116,7 +116,7 @@ export default merge.smart(baseConfig, {
           loader: 'url-loader',
           options: {
             limit: 10000,
-            mimetype: 'application/font-woff',
+            mimetype: 'application/font-woff2',
           },
         },
       },
