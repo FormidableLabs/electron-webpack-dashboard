151af51b75483f1a584a1d06d457c24a3b982a8e
Carlos Paelinck <carlos13@icloud.com>
2017-10-13 14:42:39 -0500

    Fixed sizing for different breakpoints (#46)
    * Fixed sizing for different breakpoints
    
    * Removed old container

--- app/components/Row.js
+++ app/components/Row.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export default styled.div`
   display: flex;
-  flex: 1;
+  flex: ${({ size }) => (size === 'small') ? '1 0 100%' : '1'};
   justify-content: space-around;
   flex-direction: ${({ size }) => (size === 'small' ? 'column' : 'row')};
 `;
